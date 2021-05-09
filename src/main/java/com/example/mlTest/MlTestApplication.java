c9b00711d9233040e76c3975a0f5898009a748ef
miaoliang <miaoliang01@gmail.com>
2021-05-09 19:53:37 +0800

    first commit

---
+++ src/main/java/com/example/mlTest/MlTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mlTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mlTest/MlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
